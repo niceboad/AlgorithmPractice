66c72fae8eb96a6dd366f1519ceffd65139c6a19
niceboad <79581507+niceboad@users.noreply.github.com>
2022-08-17 04:41:25 +0900

    Add files via upload

--- アルゴリズム/consumptionTaxCalc.php
+++ アルゴリズム/consumptionTaxCalc.php
@@ -6,5 +6,5 @@ function consumptionTaxCalc($price)
     echo "消費税込みで、${includingTax}円です。";
     echo PHP_EOL;
 }
-consumptionTaxCalc(1000); //商品価格がが1000円の場合
+consumptionTaxCalc(1000); //商品価格が1000円の場合
 consumptionTaxCalc(500);//商品価格が500円の場合
