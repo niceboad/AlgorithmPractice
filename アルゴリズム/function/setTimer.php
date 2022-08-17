4432af1c8e2dc5076329a53c013994e7d376e9e8
niceboad <79581507+niceboad@users.noreply.github.com>
2022-08-18 08:50:25 +0900

    Add files via upload

---
+++ アルゴリズム/function/setTimer.php
@@ -0,0 +1,12 @@
+<?php
+function setTimer($time)
+{
+    echo "時間をセットして下さい。";
+    echo PHP_EOL;
+    echo "セットされた時間は${time}分です。";
+    echo PHP_EOL;
+    echo "時間が経ちました。";
+    echo PHP_EOL;
+}
+setTimer(30);
+setTimer(50);
