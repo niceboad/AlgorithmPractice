69a3960470f51e8ddf1cc05d793b9a5e7db74710
niceboad <79581507+niceboad@users.noreply.github.com>
2022-08-20 13:49:42 +0900

    Delete at46A.php

--- アルゴリズム/AtCoder/at46A.php
+++
@@ -1,8 +0,0 @@
-<?php
-$stdin = trim(fgets(STDIN));
-$stdinArray = explode(' ', $stdin);
-$newArray = [];
-sort($stdinArray);
-$newArray = array_unique($stdinArray);
-$ans = count($newArray);
-echo $ans;
